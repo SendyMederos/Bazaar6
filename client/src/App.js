7b6bcc084dd499747db7326ce6e317db003d54c4
Evan Motte <evanwilliammotte@gmail.com>
2020-12-14 20:33:39 -0500

    setting up structure for server/routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+      <div></div>
+  )
+}
+
+export default App;
 
