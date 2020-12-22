fdf96fb5316b945b1f000c9b18da553f120c8522
SendyMederos <sendymederos@hotmail.com>
2020-12-21 21:54:27 -0500

    changes on models one time api call to get mock users

--- models/User.js
+++ models/User.js
@@ -3,36 +3,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstName: {
-    type: String,
-  },
-  lastName:{
-    type: String,
-  },
-  email:{
-    type: String,
-    unique: true
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true
 
-  },
-  phoneNumber:{
-    type: Number,
-    unique: true 
-  },
-  zipCode:{
-    type: Number,
-  },
-  order:[
-    {
-      type:Schema.TypesObjectId,
-      Ref: "Order"
-    }
-  ],
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Post"
-    }
-  ]
+    },
+    location:
+             {
+                street: {
+                    type: String
+                },
+                city:
+                {
+                    type: String
+                },
+                state:
+                {
+                    type: String
+
+                },
+                postcode:
+                {
+                    type: Number
+                },
+            },
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            Ref: "Order"
+        }
+    ],
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ]
 });
 
 const User = mongoose.model("User", UserSchema);
