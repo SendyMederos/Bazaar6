aa0b8972ae1a440e8318e8526da26b8976557796
SendyMederos <sendymederos@hotmail.com>
2020-12-21 22:51:49 -0500

    fixingthe mess

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 import Checkout from "./pages/checkout";
 import Dashboard from "./pages/dashboard";
 import Homepage from "./pages/home";
@@ -8,48 +8,44 @@ import Wanted from "./pages/wanted";
 import Product from "./pages/product";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import API from "./utils/API";
-const fs = require("fs");
-const util = require("util");
+import LocalAPI from "./utils/LocalAPI";
 
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      employees: [],
-      allEmployees: [],
-    };
-  };
- writeFile(){
-  const writeFileAsync = util.promisify(fs.writeFile);
-  
-}
-  
-  componentDidMount() {
+
+function App() {
+
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    bringingthemdown()
+  }, []);
+
+  const bringingthemdown = () => {
     API.search()
-      .then(res => res.data.results.map(result => (
-        {
-          lastName: `${result.name.last}`,
-          firstName: `${result.name.first}`,
-          location: result.location,
-          email: result.email,
-          id: res.data.results.indexOf(result) + 1
-        })))
-      .then(newData => {
-        this.setState({ allEmployees: newData, employees: newData });
-        console.log(newData)
-        console.log(this.state.allEmployees)
-      })
-      .catch(error => alert(error))
-  }
-  // The app will not render correctly until you setup a Route component.
-  // Refer to the Basic Example documentation if you need to.
-  // (https://reacttraining.com/react-router/web/example/basic)
- render(){
-   return( <Router>
-      <div>
-        <Switch>
-          {/* <Route exact path="/" component={Homepage} />
+      .then(res =>{
+        LocalAPI.saveUser(res.data.results.map(result => (
+          {
+            lastName: `${result.name.last}`,
+            firstName: `${result.name.first}`,
+            location: {
+              city: result.location.city,
+              state: result.location.state,
+              street: result.location.street.number + result.location.street.name,
+              postcode: result.location.postcode
+            },
+            email: result.email,
+            id: res.data.results.indexOf(result) + 1
+          })
+          ))
+          }).catch (error => alert(error))
+}
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+  return (<Router>
+    <div>
+      <Switch>
+        {/* <Route exact path="/" component={Homepage} />
         <Route exact path="/home" component={Homepage} />
         <Route exact path="/posting" component={Posting} />
         <Route exact path="/dashboard" component={Dashboard} />
@@ -57,10 +53,12 @@ class App extends React.Component {
         <Route exact path="/wanted" component={Wanted} />
         <Route exact path="/product" component={Product} />
         <Route exact path="/checkout" component={Checkout} /> */}
-        </Switch>
-      </div>
-    </Router>
-  )};
-}
+      </Switch>
+    </div>
+  </Router>
+  )
+};
+
+
 
 export default App;
