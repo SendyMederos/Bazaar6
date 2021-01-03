fbf4e1d34835d0b8a2b351cd48eda4c4021b0b61
bkny1020 <cgabb1020@gmail.com>
2021-01-03 14:04:10 -0500

    Fixed up a bit more class names in the Card component

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -3,18 +3,21 @@ import './style.css';
 
 export function MediaCard(props) {
 
-  return (
-      <div className="wrapper">
-        <div className="card">
-          <img src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&amp;fit=crop&amp;w=667&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
-          <div className="info">
-            <h1>Mountain</h1>
-            <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
-            <h4 className="float-right">$200</h4>
-            <button className="cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add to cart</button>
-            <button className="more"><i class="fa fa-info" aria-hidden="true"></i> More Info</button>
-          </div>
-        </div>
-      </div>
-  );
+	return (
+		<div className="wrapper">
+			<div className="card">
+				<img
+					src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&amp;fit=crop&amp;w=667&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"/>
+				<div className="info">
+					<h1>Mountain</h1>
+					<p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
+					<h4 className="float-right">$200</h4>
+					<button className="cart"><i className="fa fa-shopping-cart" aria-hidden="true"/>
+						Add to cart
+					</button>
+					<button className="more"><i className="fa fa-info" aria-hidden="true"/> More Info</button>
+				</div>
+			</div>
+		</div>
+	);
 }
