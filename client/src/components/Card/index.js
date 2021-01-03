f9f3d4e83b45da4355cc615f8f5fc99422301acf
bkny1020 <cgabb1020@gmail.com>
2021-01-03 13:52:46 -0500

    Fixed up the classnames

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -5,9 +5,9 @@ export function MediaCard(props) {
 
   return (
       <div className="wrapper">
-        <div class="card">
+        <div className="card">
           <img src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&amp;fit=crop&amp;w=667&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
-          <div class="info">
+          <div className="info">
             <h1>Mountain</h1>
             <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
             <h4 className="float-right">$200</h4>
