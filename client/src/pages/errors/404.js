fc660ff376b7c3b21cbc77dff304ba73cc296677
SendyMederos <sendymederos@hotmail.com>
2021-01-05 16:42:44 -0500

    adding cahnges to sign in and sign up pages

--- client/src/pages/errors/404.js
+++ client/src/pages/errors/404.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Error404 = ({ children }) => {
 
 	return (<>
-		<h1>404: Page Not Found</h1>
+		<h1 >404: Page Not Found</h1>
 	</>)
 
 }
--- client/src/pages/signing/index.js
+++ client/src/pages/signing/index.js
@@ -3,81 +3,64 @@ import React, { useState } from "react";
 import { Signup, Signin } from '../../components/SignForms';
 import "./style.css"
 import { makeStyles } from '@material-ui/core/styles';
-import UserContext from "../../utils/context/userContext";
-
+import { sign } from "jsonwebtoken";
 
 const useStyles = makeStyles(() => ({
-
 }));
 
 export default function Signing() {
-
-    const [amember, setAmember] = useState();
-    var  user ={};
-   
-
-    const classes = useStyles();
-
-    // const SubmitUser = () => {
-    //     console.log("submitUser")
-    //     const user = {
-    //         firstName: "Evan123456",
-    //         lastName: "56",
-    //         email: "emow456@gmail.com",
-    //         password: "1234123456",
-    //         location: {
-    //             street: "main st",
-    //             city: "pineville",
-    //             state: "nc",
-    //             postcode: "28134"
-    //         }
-    //     }
-    //     createUser(user)
-    // }
-
-    // const handleSigninTrue = () => {
-    //     SubmitUser()
-    // }
-
-    function notamember() {
-        setAmember(!amember)
+    const [isLoggingIn, setisLoggingIn] = useState(false);
+    const [loginForm, setLoginForm] = useState({
+        email: '',
+        password: '',
+    });
+    const [signUpForm, setSignUpForm] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        street: '',
+        state: '',
+        zip: '',
+        city: '',
+    });
+    const handleFormChange = (value, keys) => {
+        isLoggingIn ?
+         setLoginForm({
+                ...loginForm,
+                [keys]: value
+            }) :
+            setSignUpForm({
+                ...signUpForm,
+                 [keys]: value,
+            })
     }
-   async  function signup (e) {
-       e.preventDefault()
-       user = {
-            firstName: e.target.fristname.value,
-            lastName: e.target.lastname.value,
-            email: e.target.email.value,
-            password: e.target.password.value,
-            location: {
-                street: e.target.inputAddress.value + e.target.inputAddress2.value,
-                city: e.target.inputCity.value,
-                state: e.target.inputState.value,
-                postcode: e.target.inputZip.value,
-            }
-        }
-        console.log(user)
-        alert(user)
+    const handleFinish = () => {
+        const valueToSend = isLoggingIn ? loginForm : signUpForm;
     }
-    function login() {
-
-     }
-
     return (
-    <UserContext.Provider value={{ login, signup, notamember }}>
-            <div style={{ width: "100%" }}>
-                <div className="row">
-                    <div className="col-12 backbox">
-                        <div className="forms">
-                            <h1 className="align-center"> BAZAAR6</h1>
-                            {!amember ?<Signin /> : <Signup />}
-                        </div>
-                    </div>
-                    <div className="col-2 right">
-                        <img className="" src="https://i.pinimg.com/originals/b0/63/e6/b063e69aec55ee699cf38c757cabaae3.jpg" />
+        <div style={{ width: "100%" }}>
+            <div className="row">
+                <div className="col-12 backbox">
+                    <div className="forms">
+                        <h1 className="align-center"> BAZAAR6</h1>
+                        {!isLoggingIn ?
+                            <Signin
+                                handleFinish={handleFinish}
+                                handleFormChange={handleFormChange}
+                                loginForm={loginForm}
+                                setisLoggingIn={() => setisLoggingIn(!isLoggingIn)} /> :
+                            <Signup
+                                handleFinish={handleFinish}
+                                handleFormChange={handleFormChange}
+                                signUpForm={signUpForm}
+                                setisLoggingIn={() => setisLoggingIn(!isLoggingIn)} />}
                     </div>
                 </div>
+                <div className="col-2 right">
+                    <img className="" src="https://i.pinimg.com/originals/b0/63/e6/b063e69aec55ee699cf38c757cabaae3.jpg" />
+                </div>
             </div>
-         </UserContext.Provider>
+        </div>
     )
 }
