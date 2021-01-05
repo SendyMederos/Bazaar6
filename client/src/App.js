7bc732d5487b50715ca5f4d1a2f4de8728696c46
Evan Motte <evanwilliammotte@gmail.com>
2021-01-05 13:41:18 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,9 @@ import Signing from "./pages/signing";
 import Wanted from "./pages/wanted";
 import Product from "./pages/product";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {Error404} from "./pages/errors/404";
-import {Layout} from "./components/Layout";
-import {Posting} from "./pages/posting";
+import { Error404 } from "./pages/errors/404";
+import { Layout } from "./components/Layout";
+import { Posting } from "./pages/posting";
 // import API from "./utils/API";
 // import LocalAPI from "./utils/LocalAPI";
 
@@ -17,25 +17,25 @@ import {Posting} from "./pages/posting";
 
 function App() {
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
+  // The app will not render correctly until you setup a Route component.
+  // Refer to the Basic Example documentation if you need to.
+  // (https://reacttraining.com/react-router/web/example/basic)
   return (<Router>
-     <Switch>
-    <Route exact path="/" component={Signing} />
-    <Route path="/signing" component={Signing} />
-    <Layout>
-     
-        
+    <Switch>
+      <Route exact path="/" component={Signing} />
+      <Route path="/signing" component={Signing} />
+      <Layout>
+
+
         <Route path="/home" component={HomePage} />
         <Route path="/signup" component={Signup} />
         <Route path="/dashboard" component={Dashboard} />
 
         <Route path="/product" component={Product} />
-        
+
         <Route path="/posting" component={Posting} />
 
-        <Route component={ Error404 } />
+        <Route component={Error404} />
 
         {/* <Route exact path="/posting" component={Posting} />
         <Route exact path="/dashboard" component={Dashboard} />
@@ -43,8 +43,8 @@ function App() {
         <Route exact path="/wanted" component={Wanted} />
         
         <Route exact path="/checkout" component={Checkout} /> */}
-     
-    </Layout>
+
+      </Layout>
     </Switch>
   </Router>
   )
